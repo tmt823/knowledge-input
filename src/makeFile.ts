1396f85bfb65908310e3451aadf498da5835dfd4
tmt823 <hatsumi.kikuchi@nijibox.co.jp>
2025-04-02 08:56:40 +0900

    update: ディレクトリ作成

--- src/makeFile.ts
+++ src/makeFile.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 const makeFile = () => {
   const today = new Date();
   const filePath = format(today, "yyyy/MM/");
-  const fileName = format(filePath, "MMdd");
+  const fileName = format(today, "MMdd");
 
   fs.mkdirSync(filePath, { recursive: true });
   fs.writeFileSync(`${filePath}${fileName}.md`, "", { flag: "a" });
